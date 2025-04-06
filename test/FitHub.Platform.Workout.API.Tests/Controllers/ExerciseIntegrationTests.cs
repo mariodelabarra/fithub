0aa38d5fd85739e3b19f543686e12637e1a01b24
mariodelabarra <mario.delabarra@outlook.com>
2025-04-06 16:33:43 +1200

    Writing first integration test for getexercises endpoint

---
+++ test/FitHub.Platform.Workout.API.Tests/Controllers/ExerciseIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitHub.Platform.Workout.API;
+using FluentAssertions;
+using System.Net;
+
+namespace FitHub.Platform.Workout.API.Tests.Controllers
+{
+    public class ExerciseIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ExerciseIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_Exercises_ReturnsOk()
+        {
+            //Act
+            var response = await _client.GetAsync("/api/exercise");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
