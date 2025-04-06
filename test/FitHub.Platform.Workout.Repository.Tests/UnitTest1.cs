9460f2d6ac62b9e3bf04a474b8203c0489bc948f
mariodelabarra <mario.delabarra@outlook.com>
2025-04-06 16:18:58 +1200

    Creating unit test project for Workout

---
+++ test/FitHub.Platform.Workout.Repository.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FitHub.Platform.Workout.Repository.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/FitHub.Platform.Workout.Service.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FitHub.Platform.Workout.Service.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
