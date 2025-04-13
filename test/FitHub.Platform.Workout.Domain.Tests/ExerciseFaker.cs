b636a299c8bcf82fb2c7bceb36e0b17f28205aaf
mariodelabarra <mario.delabarra@outlook.com>
2025-04-13 15:38:52 +1200

    Adding Moq, FluentAssertions and Bogus for unit testing and finishing the GetAllAsync test

---
+++ test/FitHub.Platform.Workout.Domain.Tests/ExerciseFaker.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+
+namespace FitHub.Platform.Workout.Domain.Tests
+{
+    public class ExerciseFaker : Faker<Exercise>
+    {
+        public ExerciseFaker()
+        {
+            RuleFor(exercise => exercise.Name, faker => faker.Random.String());
+            RuleFor(exercise => exercise.Description, faker => faker.Random.String());
+            RuleFor(exercise => exercise.Type, faker => faker.PickRandom<ExerciseType>());
+        }
+    }
+}
